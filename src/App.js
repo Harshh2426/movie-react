17b76df0ad3d3af830a108e36d3a9e4e2bc40e30
Harsh Kumar <harshpatel9811838836@gmail.com>
2023-09-08 17:56:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,50 +1,53 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import MovieCard from "./component/MovieCard";
 import "./App.css";
 //42b602aa;
 const apiURL = "https://www.omdbapi.com/?apikey=42b602aa&s=";
 
-
 const App = () => {
-  const [search,setSearch] = useState("");
-  const [movie,setMovie] = useState(null);
-  const getMovie = async(movie)=>{
-    const f = await fetch(apiURL+movie);
+  const [search, setSearch] = useState("");
+  const [movie, setMovie] = useState(null);
+  const getMovie = async (movie) => {
+    const f = await fetch(apiURL + movie);
     const json = await f?.json();
     setMovie(json.Search);
-  }
+  };
 
   return (
     <div className="container">
       <div className="searchContainer">
-        <input type="text" className="searchBar" value={search} onChange={(e)=>{
-          setSearch(e.target.value);
-        }}/>
-        <button className="searchBtn" onClick={()=>{
-          getMovie(search);
-        }}>Search</button>
+        <input
+          type="text"
+          className="searchBar"
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+        />
+        <button
+          className="searchBtn"
+          onClick={() => {
+            getMovie(search);
+          }}
+        >
+          Search
+        </button>
       </div>
       <div className="result">
-        {
-
-          movie?.length > 0 ? (
-            <div className="Maincontainer">
-              {
-                movie.map((movie,index) => (
-                <MovieCard key={index} movie={movie} />
-              ))}
-            </div>
-          )
-         :
-          (
-            <div className="empty">
-              <h2>Search For a movie</h2>
-            </div>
-          )
-        }
+        {movie?.length > 0 ? (
+          <div className="Maincontainer">
+            {movie.map((movie, index) => (
+              <MovieCard key={index} movie={movie} />
+            ))}
+          </div>
+        ) : (
+          <div className="empty">
+            <h2 className="header">Search For a movie</h2>
+          </div>
+        )}
       </div>
     </div>
-  )
+  );
 };
- 
+
 export default App;
